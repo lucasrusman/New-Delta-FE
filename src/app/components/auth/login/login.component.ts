eb81c4d80c4f11d94bdb29d4cfd6a19734ab2cfd
lucasrusman <rusmanlucas@gmail.com>
2022-03-30 08:15:09 -0300

    Login working

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
-import { NgForm } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import { AuthService } from '../auth.service';
 
-import { AuthService } from "../auth.service";
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -9,14 +18,32 @@ import { AuthService } from "../auth.service";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent{
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  public showPassword: boolean = false;
+
   isLoading = false;
 
-  constructor(public authService: AuthService) {}
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private router: Router, public authService: AuthService) {}
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  ingresar() {
+    this.router.navigateByUrl('newdelta/home')
+  }
+
   onLogin(form: NgForm) {
     if (form.invalid) {
       return;
     }
     this.isLoading = true;
     this.authService.login(form.value.email, form.value.password);
+    this.ingresar()
+    ;
   }
 }
