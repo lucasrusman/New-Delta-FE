319b46ea1500d38f1e9a371170abf89ea9fb57c8
lucasrusman <rusmanlucas@gmail.com>
2022-03-30 10:19:24 -0300

    Sidenav with problems

--- src/app/components/core/core.component.ts
+++ src/app/components/core/core.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,19 +7,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./core.component.scss'],
 })
 export class CoreComponent implements OnInit {
+  @Output() sidenavClose = new EventEmitter();
+  @Output() public sidenavToggle = new EventEmitter();
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
+  public onToggleSidenav = () => {
+    this.sidenavToggle.emit();
+  }
+
+  public onSidenavClose = () => {
+    this.sidenavClose.emit();
+  }
+
   goToHomePage(){
     this.router.navigateByUrl('/newdelta/home');
+    this.onSidenavClose()
   }
 
   goToBookingPage() {
     this.router.navigateByUrl('/newdelta/reservas');
+    this.onSidenavClose()
   }
 
   logout() {
     this.router.navigateByUrl('/');
+    this.onSidenavClose()
   }
 }
