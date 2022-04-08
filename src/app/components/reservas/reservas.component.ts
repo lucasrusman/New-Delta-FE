a5b839fe8f7bab26f69395ab8606633df960d92c
Facundo Rusman <facundo.rusman.04@gmail.com>
2022-04-08 15:25:09 -0300

    Modal

--- src/app/components/reservas/reservas.component.ts
+++ src/app/components/reservas/reservas.component.ts
@@ -1,13 +1,15 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit, ViewChild } from '@angular/core';
 
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Auto } from 'src/app/models/auto';
+import { DialogData } from 'src/app/models/DialogData';
 import { BookingService } from 'src/app/services/booking.service';
 
 export interface ReservaData {
@@ -61,6 +63,8 @@ export class ReservasComponent implements AfterViewInit {
   delete !: boolean
   creado !: boolean
 
+  modelo:string = '';
+  patente:string = '';
   constructor(private readonly reservaService: BookingService, private dialog: MatDialog, private readonly router: Router, private route: ActivatedRoute) {
     this.dataSource = new MatTableDataSource();
     this.getBookings();
@@ -96,9 +100,21 @@ export class ReservasComponent implements AfterViewInit {
   }
 
   asignar(idReserva: number){
-    //TODO: Este metood deberia abrir un modal, para completar con auto (string), cuando se acepta el modal. 
-    // Se debe actualizar la base de datos.
-    console.log(idReserva);
+    const dialogRef = this.dialog.open(AsignarDialogo, {
+      width: '250px',
+      data: {modelo: this.modelo, patente: this.patente},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        const auto = new Auto({
+          auto: this.modelo
+        })
+        this.reservaService.asignarAuto(idReserva, auto).subscribe((response)=>{
+        })
+      }
+
+    });
   }
 
   cancelar(idReserva: number){
@@ -115,5 +131,38 @@ export class ReservasComponent implements AfterViewInit {
         this.getBookings();
       }
     });
+  }
+}
+
+@Component({
+  selector: 'eliminar-dialog',
+  templateUrl: 'asignar-auto.component.html',
+  //styleUrls: ['eliminar-dialog.scss'],
+})
+export class AsignarDialogo implements OnInit {
+
+  id: number = 0;
+  modelo:string = '';
+
+  constructor(public dialogRef: MatDialogRef<ReservasComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData, private reservasService: BookingService, private readonly router: Router, private route: ActivatedRoute, private readonly reservaService: BookingService) { }
+
+  ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onDelete() {
+    const auto = new Auto({
+      auto: this.modelo
+    })
+    this.reservaService.asignarAuto(1, auto).subscribe((response)=>{
+      this.onNoClick()
+    })
+    setTimeout(() => {
+      location.reload()
+    }, 500);
+
   }
 }
--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Auto } from '../models/auto';
 import { Booking } from '../models/booking';
 
 @Injectable({
@@ -14,6 +15,11 @@ export class BookingService {
 
   postBooking(booking: Booking) {
     return this.http.post(`${environment.apiBooking}/crear`, booking);
+  }
+
+
+  asignarAuto(id:number, infoAuto : Auto){
+    return this.http.post(`${environment.apiBooking}/asignar/${id}`, infoAuto)
   }
 
   cancelar(idReserva: number){
